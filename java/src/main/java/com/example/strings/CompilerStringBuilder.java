c20f713c9dc1b0f115514b0a5e1d6bf3376c48cf
fearlessBear <519376470@qq.com>
2017-05-18 16:39:01 +0800

    编译器对字符串拼接的优化

---
+++ java/src/main/java/com/example/strings/CompilerStringBuilder.java
@@ -0,0 +1,13 @@
+package com.example.strings;
+
+/**
+ * Created by cuihaichen on 17-5-18.
+ */
+
+public class CompilerStringBuilder {
+    public static void main(String[] args) {
+        String a ="";
+        a= "a"+"c"+"d"+1;
+        String b = "b"+"c"+"d"+1;
+    }
+}
