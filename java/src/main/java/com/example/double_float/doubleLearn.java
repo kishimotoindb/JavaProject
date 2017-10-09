0b49015fe6c2617bfb1185cdc235eae76e5e79c9
cuihaichen@macpro <519376470@qq.com>
2017-10-09 11:25:09 +0800

    double类型变量，在不参与计算的情况下，赋值和打印输出的值是相同的。即，虽然double类型并不能精确的表示某些数字，但是在"显示"这个层面上，是可以"精确"的。

---
+++ java/src/main/java/com/example/double_float/doubleLearn.java
@@ -0,0 +1,20 @@
+package com.example.double_float;
+
+/**
+ * Created by haichen.cui on 2017.08.28
+ */
+
+public class doubleLearn {
+    public static void main(String[] args) {
+        //虽然0.1用float并不能精确表示，但是这里赋值，不进行计算，然后直接输出，显示结果还是0.1
+//        float f = 0.1f;
+//        System.out.println(f);
+
+        String f = "0.1";
+        double result= Double.valueOf(f);
+        System.out.println(result);
+
+    }
+
+
+}
