0c6f481bc5411bba7048298889dc27e815489887
cuihaichen <cuihaichen@xiaomi.com>
2020-03-16 16:18:47 +0800

    内部类 汉诺塔

--- java/src/main/java/com/example/Basic/inherit_inner_class/WithInnerA.java
+++ java/src/main/java/com/example/Basic/inherit_inner_class/WithInnerA.java
@@ -6,10 +6,22 @@ package com.example.Basic.inherit_inner_class;
 
 public class WithInnerA {
     public WithInnerA(String s) {
+        new InnerA(1).mF = 1;
+        new InnerB().mF = 1;
     }
 
-    protected class InnerA {
+    private class InnerA {
+        /*
+         * 内部类的访问限制符只对其他类起作用，对自己的外部类不起作用。即内部类的访问限制符是用来
+         * 限制外人的，对自己的包裹类是透明的
+         */
+        private int mF;
+
         public InnerA(int i) {
         }
+    }
+
+    private static class InnerB{
+        private int mF;
     }
 }
