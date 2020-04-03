5b7064c351c7655942d6c66e89da98f6d657b116
cuihaichen <cuihaichen@xiaomi.com>
2020-04-03 19:14:57 +0800

    xor

---
+++ java/src/main/java/com/example/Basic/Operator/Xor.java
@@ -0,0 +1,13 @@
+package com.example.Basic.Operator;
+
+/**
+ * Created by cuihaichen on 2020.04.03
+ */
+public class Xor {
+    public static void main(String[] args) {
+        System.out.println(true ^ true);
+
+//        true ^ true   false
+//        true ^ false  true
+    }
+}
